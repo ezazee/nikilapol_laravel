95a17cb2e56b6e464b513e3a1f257a46d2347627
rumahac <saefudin.rumahac@gmail.com>
2022-09-26 10:41:51 +0700

    first commit

---
+++ resources/views/master/scripts.blade.php
@@ -0,0 +1,4 @@
+    <script src="/home/js/core.min.js"></script>
+    <script src="/home/js/bootstrap.min.js"></script>
+    <script src="/home/js/owl.carousel.min.js"></script>
+    <script src="/home/js/popper.min.js"></script>
